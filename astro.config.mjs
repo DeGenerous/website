ae64becbadb0e60a0b037313e6893cc6f78312ab
Petar <petar@radojevic.rs>
2023-03-07 15:13:18 +0100

    Join

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), image()]
 });
