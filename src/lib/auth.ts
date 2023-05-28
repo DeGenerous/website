e98866c841ab610e60dde46dcc4ae695275d2e03
Petar <petar@radojevic.rs>
2023-05-28 18:24:12 +0200

    Login flow

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -20,7 +20,6 @@ const get_nonce = async (): Promise<Nullable<string>> => {
     return await response.text();
 }
 
-
 export const login = async () => {
     const contracts = await with_signer();
 
@@ -37,6 +36,8 @@ export const login = async () => {
         })
     });
 
+    login_changed(response.ok);
+
     if (!response.ok) {
         alert(`Server responded with status: ${response.status}\nError: ${await response.text()}`);
         return;
@@ -48,5 +49,57 @@ export const loggedIn = async (): Promise<boolean> => {
         method: "POST",
     });
 
+    login_changed(response.ok);
+
     return response.ok;
+}
+
+export const logout = async (): Promise<void> => {
+    await fetch(`${url}/logout`, {
+        method: "POST",
+    });
+
+    login_changed(false);
+}
+
+type LoginCallback = (state: boolean) => void
+
+let callbacks: {callback: LoginCallback, once: boolean}[] = [];
+
+export const register_login_action = (callback: LoginCallback, once = false) => {
+    callbacks.push({callback, once});
+}
+
+function get_cookie(cookie_name: string): string {
+    const c_name = cookie_name + "=";
+    const cookie_decoded = decodeURIComponent(document.cookie);
+    const cookie_parts = cookie_decoded.split(';');
+    
+    for(const c of cookie_parts) {
+        c.trim();
+        if (c.indexOf(c_name) === 0) {
+            return c.substring(c_name.length, c.length);
+        }
+    }
+    return "";
+}
+
+let logged_in = get_cookie("logged") === "true";
+
+const login_changed = (state: boolean) => {
+    logged_in = state;
+    callbacks.forEach(({callback}) => callback(state));
+    callbacks = callbacks.filter(c => !c.once);
+}
+
+export const has_logged_in = (): boolean => {
+    return logged_in;
+}
+
+export const ready = (callback: () => void) => {
+    if (logged_in) {
+        callback();
+    } else {
+        register_login_action(callback, true)
+    }
 }
