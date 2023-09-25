6fcad40000de2463aa4ef1e6722b089b7662d3f6
slobodan <slobodan.sredojevic@roundcube.rs>
2023-09-25 09:02:35 +0200

    All boxes dashes

--- src/components/roadmap/components/Roadmap.svelte
+++ src/components/roadmap/components/Roadmap.svelte
@@ -2,7 +2,6 @@
   import Box from "./Box.svelte";
   import Store from "../stores/Store";
   import Svg from "./Svg.svelte";
-  import SvgLine from "./SvgLine.svelte";
   import { onMount } from "svelte";
 
   let activeBox = 1;
@@ -37,6 +36,7 @@
 
   let boxInterval = setInterval(increaseBox, nextBoxTime);
 
+  // Drawing SVG
   let containerWidth;
   let containerHeight;
 
@@ -56,47 +56,43 @@
       let x1 = $Store[i].left;
       let y1 = $Store[i].top;
       let x2 = $Store[i + 1].left;
-      let y2 = $Store[i + 1].right;
-      console.log(`x1 = ${x1}`);
+      let y2 = $Store[i + 1].top;
+
+      // Difference beetween points
+      let dx = x2 - x1;
+      dx = (parseFloat(dx) / 100) * containerWidth;
+
+      let dy = y2 - y1;
+      dy = (parseFloat(dy) / 100) * containerHeight;
+
+      // Total length between points
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      // Max number of steps between points
+      const numSteps = Math.floor(distance / svgLength);
+
+      //Steps for x and y
+      let stepX = dx / numSteps;
+      let stepY = dy / numSteps;
+
+      // Generating points beetween boxes
+      for (let i = 1; i < numSteps; i++) {
+        const newX =
+          (parseFloat(x1) / 100) * containerWidth + i * stepX + boxRadius;
+        const newY =
+          (parseFloat(y1) / 100) * containerHeight + i * stepY + boxRadius;
+
+        points.push({ x: newX, y: newY });
+      }
+
+      // Taking every third pair for drawing SVG
+      for (let i = 0; i < points.length - 1; i++) {
+        let firstPoint = points[i];
+        let secondPoint = points[i + 1];
+        if (i % 3 === 0) pairs.push({ firstPoint, secondPoint });
+      }
     }
-
-    let x1 = 10;
-    let y1 = 60;
-    let x2 = 60;
-    let y2 = 20;
-    // Difference beetween points
-    let dx = x2 - x1;
-    dx = (parseFloat(dx) / 100) * containerWidth;
-
-    let dy = y2 - y1;
-    dy = (parseFloat(dy) / 100) * containerHeight;
-
-    // Total length between points
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    // Max number of steps between points
-    const numSteps = Math.floor(distance / svgLength);
-
-    //Steps for x and y
-    let stepX = dx / numSteps;
-    let stepY = dy / numSteps;
-
-    // Generating points beetween boxes
-    for (let i = 1; i < numSteps; i++) {
-      const newX =
-        (parseFloat(x1) / 100) * containerWidth + i * stepX + boxRadius;
-      const newY =
-        (parseFloat(y1) / 100) * containerHeight + i * stepY + boxRadius;
-
-      points.push({ x: newX, y: newY });
-    }
-
-    // Taking every third pair for drawing SVG
-    for (let i = 0; i < points.length - 1; i++) {
-      let firstPoint = points[i];
-      let secondPoint = points[i + 1];
-      let id = i;
-      if (i % 3 === 0) pairs.push({ firstPoint, secondPoint, id });
-    }
+    console.log(pairs.length);
   });
 </script>
 
@@ -107,7 +103,7 @@
   }}
 >
   {#if containerHeight}
-    {#each pairs as pair (pair.id)}
+    {#each pairs as pair}
       <Svg
         x1={pair.firstPoint.x}
         y1={pair.firstPoint.y}
--- src/components/roadmap/components/Svg.svelte
+++ src/components/roadmap/components/Svg.svelte
@@ -6,15 +6,11 @@
 </script>
 
 <svg class="svg-container" height="1000" width="1000">
-  <line {x1} {y1} {x2} {y2} stroke="green" stroke-width="3" />
+  <line {x1} {y1} {x2} {y2} stroke="grey" stroke-width="3" />
 </svg>
 
 <style>
   .svg-container {
     position: absolute;
-    /* top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%; */
   }
 </style>
