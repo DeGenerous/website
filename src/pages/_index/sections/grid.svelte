0dacf1aba0728c23a1d149d44176f830e65dcf57
Sniff <sniffdk@gmail.com>
2024-03-13 23:39:47 +0100

    Updated Potentials banner image, updated Sagaverse name

--- src/pages/_index/sections/grid.svelte
+++ src/pages/_index/sections/grid.svelte
@@ -26,9 +26,9 @@
 
 <section class="potentials columns">
     <div class="image-grid">
-        <ImageButton my_state={HomeSection.Potentials} image="/images/potentials/potentials.png">Potentials</ImageButton>
+        <ImageButton my_state={HomeSection.Potentials} image="/images/potentials/potentials2.png">Potentials</ImageButton>
         <ImageButton my_state={HomeSection.CoNexus} image="/images/potentials/conexus.jpg">CoNexus</ImageButton>
-        <ImageButton my_state={HomeSection.SagaChallenges} image="/images/potentials/saga_gaming.png">Saga Challenges</ImageButton>
+        <ImageButton my_state={HomeSection.SagaChallenges} image="/images/potentials/saga_gaming.png">Sagaverse Gaming</ImageButton>
         <ImageButton my_state={HomeSection.DischordianSaga} image="/images/potentials/dischordian.png">The Dischordian Saga</ImageButton>
         <ImageButton my_state={HomeSection.Philanthropy} image="/images/potentials/philanthropy.png">Philanthropy</ImageButton>
         <ImageButton my_state={HomeSection.Vision} image="/images/potentials/vision.png">Vision</ImageButton>
