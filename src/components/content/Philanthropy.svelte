fa53d2d22629ff78924ce200fbe6b59749fb2c7d
Dimon <notawid@gmail.com>
2025-03-19 21:29:55 +0100

    philanthropy rebuilding

--- src/components/content/Philanthropy.svelte
+++ src/components/content/Philanthropy.svelte
@@ -3,39 +3,45 @@
 <h1>Supercharging Giving</h1>
 
 <section class="flex-box blur">
-  <h3>
-    At DeGenerous DAO, we believe that philanthropy should be transparent,
-    engaging, and impactful. With our initial NFT sale raising $125K for
-    charity, we've already demonstrated the power of Web3 to drive real-world
-    change. Thanks to our partnership with Lions Clubs International, we ensure
-    that 100% of funds raised for charity go directly to those in need through
-    well-organized, co-branded global service missions. Our vision is to gamify
-    giving, allowing supporters to earn unique NFTs, traits, and aesthetics as
-    proof of their contributions—each digital asset telling a story of
-    generosity, woven into a larger movement of social good.
-  </h3>
+  <div class="flex-box">
+    <img src="/museum.jpg" alt="Phylanthropy" />
+    <article class="flex-box">
+      <h2>Proof-Of-Impact: Turning Digital Contributions into Real Change</h2>
+      <h3>
+        At DeGenerous DAO, we believe that philanthropy should be transparent,
+        engaging, and impactful. With our initial NFT sale raising $125K for
+        charity, we've already demonstrated the power of Web3 to drive
+        real-world change. Thanks to our partnership with Lions Clubs
+        International, we ensure that 100% of funds raised for charity go
+        directly to those in need through well-organized, co-branded global
+        service missions. Our vision is to gamify giving, allowing supporters to
+        earn unique NFTs, traits, and aesthetics as proof of their
+        contributions—each digital asset telling a story of generosity, woven
+        into a larger movement of social good.
+      </h3>
+    </article>
+  </div>
 
   <hr />
 
-  <h2>Proof-Of-Impact: Turning Digital Contributions into Real Change</h2>
-
   <div class="flex-box">
-    <img src="/philanthropy.webp" alt="Lions International" />
-    <h3>
-      Philanthropy isn’t just about donations—it’s about measurable,
-      life-changing impact. From funding international service trips to
-      empowering communities, DeGenerous DAO is committed to ensuring that every
-      contribution translates into direct action. As humanitarian partners of
-      Lions International, the world’s largest service organization with 1.4M
-      members and 50K local clubs, we’ve helped fund multiple $100K+ global
-      service projects, including a major initiative in India in February 2024,
-      where our community shaped the mission through innovative governance
-      systems. True impact isn’t measured in bytes, but in smiles, shared
-      stories, and lives forever changed.
-    </h3>
+    <img src="/ball.jpg" alt="Phylanthropy" />
+    <article class="flex-box">
+      <h2>Make the World a Better Place. Unlock Your Potential.</h2>
+      <h3>
+        Philanthropy isn’t just about donations—it’s about measurable,
+        life-changing impact. From funding international service trips to
+        empowering communities, DeGenerous DAO is committed to ensuring that
+        every contribution translates into direct action. As humanitarian
+        partners of Lions International, the world’s largest service
+        organization with 1.4M members and 50K local clubs, we’ve helped fund
+        multiple $100K+ global service projects, including a major initiative in
+        India in February 2024, where our community shaped the mission through
+        innovative governance systems. True impact isn’t measured in bytes, but
+        in smiles, shared stories, and lives forever changed.
+      </h3>
+    </article>
   </div>
-
-  <h2>Make the World a Better Place. Unlock Your Potential.</h2>
 </section>
 
 <style>
@@ -51,8 +57,8 @@
     gap: 2vw;
   }
 
-  div h3 {
-    text-align: left;
+  article {
+    gap: 1vw;
   }
 
   img {
@@ -74,10 +80,6 @@
     div {
       flex-direction: column;
       gap: 1em;
-    }
-
-    div h3 {
-      text-align: center;
     }
 
     img {
