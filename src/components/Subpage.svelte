ba30d4697f9bfc9aa677ede5e3adcc73d1aeb8bc
Dimon <notawid@gmail.com>
2025-05-08 20:59:09 +0200

    created subpage for collaborations

--- src/components/Subpage.svelte
+++ src/components/Subpage.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   export let title: string = "";
+  export let project: Nullable<Collab> = null;
+
   let scroll: number;
 </script>
 
@@ -37,4 +39,242 @@
 
 <slot />
 
+{#if project}
+  <p class="collab-description">{project.description}</p>
+
+  {#if project.logo}
+    <img class="collab-logo" src={project.logo} alt={project.name} />
+  {/if}
+
+  <section class="blur tiles-collection">
+    {#each project.stories as { picture, title, link }}
+      <a class="tile" href={link} target="_blank">
+        <img class="tile-picture" src={picture} alt={title} />
+        <p class="tile-title">{title}</p>
+      </a>
+    {/each}
+  </section>
+
+  <div class="flex-box blur collab-links">
+    <h3>Links:</h3>
+    {#if project.website}
+      <a href={project.website} target="_blank">
+        <img class="link-icon" src="/icons/website.png" alt="Website" />
+      </a>
+    {/if}
+    {#if project.twitter}
+      <a href={project.twitter} target="_blank">
+        <img class="link-icon" src="/icons/twitter.png" alt="Twitter" />
+      </a>
+    {/if}
+    {#if project.tiktok}
+      <a href={project.tiktok} target="_blank">
+        <img class="link-icon" src="/icons/tiktik.png" alt="TikTok" />
+      </a>
+    {/if}
+    {#if project.telegram}
+      <a href={project.telegram} target="_blank">
+        <img class="link-icon" src="/icons/telegram.png" alt="Telegram" />
+      </a>
+    {/if}
+    {#if project.discord}
+      <a href={project.discord} target="_blank">
+        <img class="link-icon" src="/icons/discord.png" alt="Discord" />
+      </a>
+    {/if}
+    {#if project.warpcast}
+      <a href={project.discord} target="_blank">
+        <img class="link-icon" src="/icons/warpcast.png" alt="Warpcast" />
+      </a>
+    {/if}
+    {#if project.youtube}
+      <a href={project.youtube} target="_blank">
+        <img class="link-icon" src="/icons/youtube.png" alt="YouTube" />
+      </a>
+    {/if}
+  </div>
+{/if}
+
 <div class="background subpage-bg" style:top={-scroll / 25 + "vh"}></div>
+
+<style>
+  .collab-description {
+    width: 90%;
+    font-size: 1.5vw;
+    line-height: 2.5vw;
+    text-align: center;
+    color: rgba(51, 226, 230, 0.75);
+    text-shadow: 0 0.25vw 0.25vw #010020;
+  }
+
+  .collab-logo {
+    width: 25vw;
+  }
+
+  .tiles-collection {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    gap: 1vw;
+    padding: 2vw;
+    background-color: rgba(1, 0, 32, 0.75);
+    box-shadow:
+      inset 0 0 0.5vw rgba(51, 226, 230, 0.25),
+      0 0 0.5vw #010020;
+    overflow-y: hidden;
+  }
+
+  .tiles-collection::-webkit-scrollbar {
+    height: 0.75vw;
+  }
+
+  .tiles-collection::-webkit-scrollbar-track {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  .tiles-collection::-webkit-scrollbar-thumb {
+    background: linear-gradient(
+      to right,
+      rgba(51, 226, 230, 0.15),
+      rgba(51, 226, 230, 0.3),
+      rgba(51, 226, 230, 0.15)
+    );
+    border-radius: 1vw;
+    cursor: pointer;
+  }
+
+  .tiles-collection::-webkit-scrollbar-thumb:hover,
+  .tiles-collection::-webkit-scrollbar-thumb:active {
+    background: rgba(51, 226, 230, 0.5);
+  }
+
+  .tile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 20vw;
+    background-color: rgba(36, 65, 189, 0.75);
+    color: rgba(255, 255, 255, 0.75);
+    border: 0.1vw solid rgba(51, 226, 230, 0.5);
+    border-radius: 1.5vw;
+    box-shadow: 0 0 0.5vw #010020;
+    cursor: pointer;
+    text-decoration: none;
+    flex: none;
+    padding: 0;
+  }
+
+  .tile:hover,
+  .tile:active {
+    background-color: rgba(45, 90, 216, 0.9);
+    color: rgba(51, 226, 230, 0.9);
+    box-shadow: 0 0.5vw 0.5vw #010020;
+    transform: scale(1.025) translateY(-0.5vw);
+    text-decoration: none;
+  }
+
+  .tile-picture {
+    object-fit: cover;
+    width: 95%;
+    height: 19vw;
+    margin: 2.5%;
+    margin-bottom: 0;
+    border: 0.1vw solid rgba(51, 226, 230, 0.5);
+    border-radius: 1vw;
+    flex: 1;
+    background-color: rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+  }
+
+  .tile-title {
+    text-align: center;
+    padding: 1vw;
+    font-size: 2vw;
+    line-height: 3vw;
+    text-shadow: 0 0 1vw rgba(1, 0, 32, 0.4);
+    cursor: pointer;
+  }
+
+  .collab-links {
+    flex-flow: row;
+    align-items: center;
+    gap: 1.5vw;
+    padding: 1.5vw;
+    border-radius: 1.5vw;
+    background-color: rgba(51, 226, 230, 0.1);
+    box-shadow: 0 0 0.5vw #010020;
+  }
+
+  .link-icon {
+    width: 4vw;
+    height: 4vw;
+    border-radius: 20%;
+  }
+
+  .link-icon:hover,
+  .link-icon:active {
+    transform: scale(1.1);
+    filter: brightness(125%);
+  }
+
+  @media only screen and (max-width: 600px) {
+    .collab-description {
+      font-size: inherit;
+      line-height: 1.5em;
+      padding: 1em;
+      background-color: rgba(22, 30, 95, 0.5);
+      -webkit-backdrop-filter: blur(2vw);
+      backdrop-filter: blur(2vw);
+      box-shadow:
+        inset 0 0 0.5vw rgba(51, 226, 230, 0.5),
+        0 0 0.5vw #010020;
+      border-radius: 1em;
+    }
+
+    .collab-logo {
+      width: 75vw;
+    }
+
+    .tiles-collection {
+      border-radius: 0;
+      border-left: none;
+      border-right: none;
+      gap: 1em;
+      padding: 1em;
+    }
+
+    .tile {
+      width: 40vw;
+      border-radius: 1em;
+    }
+
+    .tile-picture {
+      height: 38vw;
+      border-radius: 0.75em;
+    }
+
+    .tile-title {
+      font-size: 1em;
+      line-height: 1.5em;
+    }
+
+    .collab-links {
+      width: 100vw;
+      gap: 1em;
+      padding: 1em;
+      border-radius: 0;
+    }
+
+    .collab-links a {
+      padding: 0;
+    }
+
+    .link-icon {
+      width: 2.5em;
+      height: 2.5em;
+    }
+  }
+</style>
---
+++ src/constants/collabs.ts
@@ -0,0 +1,58 @@
+const mediaURL = (collabName: string, type: "logo" | "bg" = "logo") =>
+  `/collabs/subpages/${collabName}/${type}.webp`;
+
+const tilePictureURL = (collabName: string, picture: string) =>
+  `/collabs/subpages/${collabName}/${picture}.webp`;
+
+const storyURL = "https://conexus.degenerousdao.com/sections/Collabs";
+
+const collabs: Map<string, Collab> = new Map();
+
+collabs.set('glmr-apes', {
+  name: "GLMR Apes",
+  description: "GLMR Apes is a community-driven NFT initiative on the Moonbeam network, featuring a collection of 1001 pixel-art apes. As the first sold-out NFT collection on Moonbeam, GLMR Apes established the network's inaugural DAO, empowering holders with governance rights through the \"Apemocracy.\" The project has expanded to include a second NFT series, GLMR Jungle, and launched \"The Great Escape,\" a web3 play-to-earn game.",
+  logo: mediaURL("glmr-apes"),
+  background: mediaURL("glmr-apes", "bg"),
+  stories: [
+    {
+      picture: tilePictureURL("glmr-apes", "glmr-apes"),
+      title: "GLMR Apes",
+      link: `${storyURL}/GLMR%20Apes?id=89`,
+    }
+  ],
+  website: "https://www.glmrapes.com/",
+  twitter: "https://x.com/GLMRApes",
+  telegram: "https://t.me/+ziGQHawfZ3ZhYTk0",
+  discord: "https://discord.com/invite/keJS7k8MUT"
+})
+
+collabs.set('anyone', {
+  name: "ANyONe",
+  description: "Anyone is a decentralized privacy network that routes internet traffic through encrypted, multi-hop circuits using incentivized relays. It offers a modular privacy layer for applications and users, ensuring anonymity without relying on centralized options.",
+  logo: mediaURL("anyone"),
+  background: mediaURL("anyone", "bg"),
+  stories: [
+    {
+      picture: tilePictureURL("anyone", "the-relay-guide"),
+      title: "The Relay Guide",
+      link: `${storyURL}`,
+    },
+    {
+      picture: tilePictureURL("anyone", "router-mode"),
+      title: "Router Mode - Anyone’s Hotspot",
+      link: `${storyURL}`,
+    },
+    // {
+    //   picture: tilePictureURL("anyone", "the-anon-uprising"),
+    //   title: "The Anon Uprising",
+    //   link: `${storyURL}`,
+    // }
+  ],
+  website: "https://www.anyone.io/",
+  twitter: "https://x.com/AnyoneFDN",
+  telegram: "https://t.me/anyoneprotocol",
+  discord: "https://discord.com/invite/anyoneprotocol",
+  youtube: "http://youtube.com/@anyoneprotocol"
+})
+
+export default collabs;
