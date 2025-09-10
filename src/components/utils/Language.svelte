51e9a3e666afef1e4272b9445ae1fa88e85b9d60
Dimon <notawid@gmail.com>
2025-09-10 18:34:58 +0200

    added all languages

--- src/components/utils/Language.svelte
+++ src/components/utils/Language.svelte
@@ -1,44 +1,14 @@
 <svelte:head>
-  <script>
-    window.gtranslateSettings = {
-      default_language: "en",
-      native_language_names: true,
-      detect_browser_language: true,
-      languages: [
-        "en",
-        "fr",
-        "it",
-        "es",
-        "ar",
-        "zh-CN",
-        "nl",
-        "de",
-        "hi",
-        "ja",
-        "ko",
-        "pl",
-        "pt",
-        "ru",
-        "id",
-        "th",
-        "tr",
-        "vi",
-        "uk",
-        "fa",
-        "sr",
-        "cs",
-        "no",
-      ],
-      globe_color: "#3875fa",
-      wrapper_selector: ".gtranslate_wrapper",
-      flag_size: 24,
-      globe_size: 40,
-    };
-  </script>
+  <script>window.gtranslateSettings = {
+    "default_language":"en",
+    "native_language_names":true,
+    "detect_browser_language":true,
+    "globe_color":"#3875fa",
+    "wrapper_selector":".gtranslate_wrapper",
+    "flag_size":24,
+    "globe_size":40
+  }</script>
   <script src="https://cdn.gtranslate.net/widgets/latest/globe.js" defer></script>
 </svelte:head>
 
 <div class="gtranslate_wrapper"></div>
-
-<style>
-</style>
